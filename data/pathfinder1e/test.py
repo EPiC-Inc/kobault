43a75d3e1620b1f2b527f7122bfdc480d8104124
Emmett Watkins-Brown <emmettwb@gmail.com>
2023-02-07 20:30:07 -0700

    taking spells out of the sidebar

--- data/pathfinder1e/test.py
+++ data/pathfinder1e/test.py
@@ -3,12 +3,12 @@ import json
 with open('data/pathfinder1e/skills.json', 'r') as skillsfile:
     skills = json.load(skillsfile)
 
-for skill in "Acrobatics | Appraise | Bluff | Climb | Craft | Diplomacy | Disable Device | Disguise | Escape Artist | Fly | Handle Animal | Heal | Intimidate | Knowledge | Linguistics | Perception | Perform | Profession | Ride | Sense Motive | Sleight of Hand | Spellcraft | Stealth | Survival | Swim | Use Magic Device".split(" | "):
-    skills[skill] = {
-        "ability": "intelligence",
-        "trained": False,
-        "blank": False,
-    }
+# for skill in "Acrobatics | Appraise | Bluff | Climb | Craft | Diplomacy | Disable Device | Disguise | Escape Artist | Fly | Handle Animal | Heal | Intimidate | Knowledge | Linguistics | Perception | Perform | Profession | Ride | Sense Motive | Sleight of Hand | Spellcraft | Stealth | Survival | Swim | Use Magic Device".split(" | "):
+#     skills[skill] = {
+#         "ability": "intelligence",
+#         "trained": False,
+#         "blank": False,
+#     }
 
 with open('data/pathfinder1e/skills.json', 'w') as condfile:
     condfile.write(json.dumps(skills))
