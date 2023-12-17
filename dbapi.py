dab09ec1e5d7ae187748655b8144cb7c90023642
labs <emmettwb@gmail.com>
2023-12-17 00:17:37 -0700

    mutex

--- dbapi.py
+++ dbapi.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from sqlite3 import connect
+from threading import Lock
 
 DB_PATH = Path("data/i.db")
 if not DB_PATH.exists():
@@ -11,9 +12,11 @@ if not DB_PATH.exists():
 
 class Database:
     database: str
+    lock: Lock
 
     def __init__(self) -> None:
         Database.database = str(DB_PATH)
+        Database.lock = Lock()
 
 
 class Table:
@@ -26,6 +29,7 @@ class Table:
         self, columns, where_column: str | None = None, where_data: list | None = None
     ) -> list:
         """Queries the table's current data."""
+        Database.lock.acquire()
         with connect(Database.database) as connection:
             cursor = connection.cursor()
             if where_column and where_data:
@@ -35,17 +39,20 @@ class Table:
                 )
             else:
                 result = cursor.execute(f"SELECT {columns} FROM {self.name}")
+        Database.lock.release()
         return [*result]
 
-    def insert(self, values) -> None:
-        """Inserts <values> into the current table.
-        Not currently implemented.
-        """
+    def insert(self, values, *, lock_needed: bool = True) -> None:
+        """Inserts <values> into the current table."""
+        if lock_needed:
+            Database.lock.acquire()
         with connect(Database.database) as connection:
             cursor = connection.cursor()
             command = f"INSERT INTO {self.name} VALUES({('?, ' * len(values))[:-2]})"
             cursor.execute(command, values)
             connection.commit()
+        if lock_needed:
+            Database.lock.release()
 
     def update_property(
         self,
@@ -55,18 +62,23 @@ class Table:
         where_data: str,
     ) -> None:
         """Updates a SINGLE property."""
+        Database.lock.acquire()
         with connect(Database.database) as connection:
             cursor = connection.cursor()
             command = f"UPDATE {self.name} SET {property_column} = ? WHERE {where_column} = ?"
             cursor.execute(command, [str(property_data), str(where_data)])
             connection.commit()
+        Database.lock.release()
+
 
     def insert_object(self, object_) -> None:
         """Inserts an object or its properties into the current table."""
+        Database.lock.acquire()
         try:
             data = object_.__dict__
             if not data:
                 data = object_.__slots__
         except AttributeError:
             data = object_.__slots__
-        self.insert([str(getattr(object_, attr)) for attr in data])
+        self.insert([str(getattr(object_, attr)) for attr in data], lock_needed=False)
+        Database.lock.release()
