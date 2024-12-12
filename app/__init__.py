26fb2c8e992a010e8ecf3567043c67ebf89ac7f4
Emmett Watkins-Brown <emmettwb@gmail.com>
2024-12-12 03:33:42 -0700

    fewer files this time

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
-from .main import app
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(openapi_url=None)
+templates = Jinja2Templates(Path(__file__).parent / "templates")
+TemplateResponse = templates.TemplateResponse
+
+@app.get("/")
+def index():
+    return "Base :3"
