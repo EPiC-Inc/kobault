70ac58ec9723a211a9599ef94ebdcbd06ee79598
Emmett Watkins-Brown <emmettwb@gmail.com>
2024-12-12 03:46:51 -0700

    more skeleton

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,16 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
+from .fetch import fetch
+
 app = FastAPI(openapi_url=None)
 templates = Jinja2Templates(Path(__file__).parent / "templates")
 TemplateResponse = templates.TemplateResponse
 
+
 @app.get("/")
 def index():
-    return "Base :3"
+    return (Path(__file__).parent.parent / "data").exists()
+
+
+app.mount("/fetch", fetch)
---
+++ app/fetch.py
@@ -0,0 +1,3 @@
+from fastapi import FastAPI
+
+fetch = FastAPI()
